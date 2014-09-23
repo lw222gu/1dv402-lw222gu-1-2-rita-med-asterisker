9a449655e1c028dfb65ecc114ece56bfa372d01e
Lisa Westlund <lw222gu@student.lnu.se>
2014-09-23 14:24:11 +0200

    Slutlig version som redovisas
    Fått ordning på switchsatsen

--- 1dv405-lw222gu-1-2-rita-med-asterisker/1dv402.S1.L02A/1dv402.S1.L02A/Program.cs
+++ 1dv405-lw222gu-1-2-rita-med-asterisker/1dv402.S1.L02A/1dv402.S1.L02A/Program.cs
@@ -10,22 +10,35 @@ namespace _1dv402.S1.L02A
     {
         static void Main(string[] args)
         {
-            
             for (int row = 1; row <= 25; row++)
-            {
-                if (row == 2)
+            {    
+                for (int column = 1; column <= 39; column++)
+                {
+                    switch (row % 3)
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                    }
+                    Console.Write("* ");
+
+                }
+                Console.WriteLine();
+
+                if (row % 2 != 0)
                 {
                     Console.Write(" ");
-                } 
-                
-                for (int col = 1; col <= 39; col++)
-                {
-                    Console.Write(" *");                    
                 }
 
-               
+            
             } 
-            Console.WriteLine();
+            
             
         }
     }
